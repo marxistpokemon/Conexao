2c031a774a8694e531a55776f209ee15669a1552
marxistpokemon <enric.llagostera@gmail.com>
2014-03-18 20:32:22 -0300

    GDD primeiro jogável, limpeza no projeto.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -18,20 +18,65 @@ public class GameManager : MonoBehaviour {
 
 	public float meshOffsetZ = 1;
 
+	public int level;
+	public int points;
+
+	public Transform piecePrefab;
+	public Transform txtLevel;
+	public Transform txtPoints;
+	private GUIText msgLevel;
+	private GUIText msgPoints;
+
 	void Start () {
 		g = this;
 		pieceSequence = new List<Transform>();
 		connectionLine = GetComponent<LineRenderer>();
 		polCollider = GetComponent<PolygonCollider2D>();
-
-		allPieces = new List<Transform>();
-		GameObject[] allGO = GameObject.FindGameObjectsWithTag("Piece");
-		foreach (var go in allGO) {
-			allPieces.Add(go.transform);
-		}
+		UpdateAllPieces();
 		polCollider.enabled = false;
+		msgPoints = txtPoints.GetComponent<GUIText>();
+		msgLevel = txtLevel.GetComponent<GUIText>();
 	}
 
+	public Vector3 CreateMesh(Vector2[] vertices2D){
+		// Use the triangulator to get indices for creating triangles
+		Triangulator tr = new Triangulator(vertices2D);
+		int[] indices = tr.Triangulate();
+		// Create the Vector3 vertices
+		Vector3[] vertices = new Vector3[vertices2D.Length];
+		for (int i=0; i<vertices.Length; i++) {
+			vertices[i] = new Vector3(vertices2D[i].x, vertices2D[i].y, meshOffsetZ);
+		}
+		// Create the mesh
+		Mesh msh = new Mesh();
+		msh.vertices = vertices;
+		msh.triangles = indices;
+		msh.RecalculateNormals();
+		msh.RecalculateBounds();
+		msh.uv = vertices2D;
+		GetComponent<MeshFilter>().mesh = msh;
+		// returns the center so we can use it to place the combined piece
+		return msh.bounds.center;
+	}
+
+	public bool AddPieceToSeq(Transform piece){
+		int index = pieceSequence.FindIndex(p => p == piece);
+		if (index < 0) {
+			pieceSequence.Add(piece);
+			UpdateClicked();
+			return true;
+		}
+		return false;
+	}
+
+	public void RemovePieceFromSeq(Transform piece){
+		Debug.Log ("remove piece");
+		int indexToRemove = pieceSequence.FindIndex(p => p == piece);
+		pieceSequence.RemoveRange(indexToRemove, SeqCount()-indexToRemove);
+		UpdateClicked();
+	}
+
+	#region Updates and helper functions
 	void Update(){
 		polCollider.enabled = (pieceSequence.Count > 2);
 		if(pieceSequence.Count > 0){
@@ -47,58 +92,24 @@ public class GameManager : MonoBehaviour {
 				pieceSequence.Count, 
 				Camera.main.ScreenToWorldPoint(Input.mousePosition));
 		}
+		// GUI update
+		msgLevel.text = "Level: " + level;
+		msgPoints.text = "Points: " + points;
 	}
 
-	public void CreateMesh(Vector2[] vertices2D){
-		// Use the triangulator to get indices for creating triangles
-		Triangulator tr = new Triangulator(vertices2D);
-		int[] indices = tr.Triangulate();
-		
-		// Create the Vector3 vertices
-		Vector3[] vertices = new Vector3[vertices2D.Length];
-		for (int i=0; i<vertices.Length; i++) {
-			vertices[i] = new Vector3(vertices2D[i].x, vertices2D[i].y, meshOffsetZ);
+	void UpdateAllPieces(){
+		allPieces = new List<Transform>();
+		Object[] allGO = GameObject.FindObjectsOfType(typeof(Piece));
+		foreach (var go in allGO) {
+			allPieces.Add((go as Piece).transform);
 		}
-		
-		// Create the mesh
-		Mesh msh = new Mesh();
-		msh.vertices = vertices;
-		msh.triangles = indices;
-		msh.RecalculateNormals();
-		msh.RecalculateBounds();
-		msh.uv = vertices2D;
-		GetComponent<MeshFilter>().mesh = msh;
 	}
 
-	public int SequenceCount(){
-		return pieceSequence.Count;
-	}
-
-	public bool AddPiece(Transform piece){
-		int index = pieceSequence.FindIndex(p => p == piece);
-		if (index < 0) {
-			pieceSequence.Add(piece);
-			UpdateClicked();
-			return true;
-		}
-		return false;
-	}
-
-	public void RemovePiece(Transform piece){
-		int indexToRemove = pieceSequence.FindIndex(p => p == piece);
-		Debug.Log (indexToRemove);
-		pieceSequence.RemoveRange(
-			indexToRemove,
-			pieceSequence.Count - indexToRemove);
-		UpdateClicked();
-	}
-
-	public void UpdateClicked ()
+	void UpdateClicked ()
 	{
-		allPieces.ForEach(p => p.GetComponent<Piece>().isClicked = false);
-		foreach (var piece in pieceSequence) {
-			piece.GetComponent<Piece>().isClicked = true;
-		};
+		allPieces.ForEach(p => {
+			p.GetComponent<Piece>().isClicked = isInSeq(p);
+		});
 	}
 
 	public void UpdateConnectionLine ()
@@ -114,26 +125,46 @@ public class GameManager : MonoBehaviour {
 		polCollider.SetPath(0, points);
 	}
 
-	public IEnumerator CompleteSequence(Transform firstPiece){
-		moveLock = true;
-		CreateMesh(polCollider.points);
-		yield return new WaitForSeconds(completionDelay);
-		int sequenceValue = 0;
-		CreateMesh(new Vector2[3]);
-		foreach (var pieceCapture in allPieces.FindAll(
-			p => p.GetComponent<Piece>().onCaptureArea)) {
-			Debug.Log("Pontos peca: " + pieceCapture.GetComponent<Piece>().value);
-			sequenceValue += pieceCapture.GetComponent<Piece>().value;
-			allPieces.Remove(pieceCapture);
-			Destroy(pieceCapture.gameObject);
-		}
-		pieceSequence.Clear();
-		UpdateConnectionLine();
-		Debug.Log ("Total: " + sequenceValue);
-		moveLock = false;
+	public int SeqCount(){
+		return pieceSequence.Count;
 	}
 
-	public bool isInSequence(Transform piece){
-		return (pieceSequence.FindIndex(p => p == piece) > 0);
+	public bool isInSeq(Transform piece){
+		return (pieceSequence.FindIndex(p => p == piece) >= 0);
+	}
+	#endregion
+
+	public IEnumerator CompleteSeq(Transform firstPiece){
+		moveLock = true;
+		Vector3 spawnPos = CreateMesh(polCollider.points);
+		yield return new WaitForSeconds(completionDelay);
+		// clear area mesh
+		CreateMesh(new Vector2[3]);
+		// handle captured pieces
+		int capturedValue = 0;
+		int seqValue = 0;
+		allPieces.ForEach(p => {
+			Piece piece = p.GetComponent<Piece>();
+			capturedValue += (piece.onCaptureArea)? piece.value : 0;
+			seqValue += (piece.isClicked)? piece.value : 0;
+			if(piece.isClicked || piece.onCaptureArea){
+				DestroyObject(p.gameObject);
+			}
+			piece.isClicked = false;
+		});
+		points += seqValue;
+		if(capturedValue > 0){
+			Transform combinedPiece = 
+				Instantiate (piecePrefab, spawnPos, Quaternion.identity) 
+					as Transform;
+			combinedPiece.GetComponent<Piece>().value = capturedValue;
+		}
+		Debug.Log("Combined: " + capturedValue + " | Points: " + seqValue);
+		pieceSequence.Clear();
+		allPieces = null;
+		yield return new WaitForSeconds(0);
+		UpdateAllPieces();
+		UpdateConnectionLine();
+		moveLock = false;
 	}
 }
--- Assets/Scripts/Piece.cs
+++ Assets/Scripts/Piece.cs
@@ -4,15 +4,20 @@ using System.Collections;
 public class Piece : MonoBehaviour {
 
 	public int value = 0;
+	public Sprite[] pieceGraphics;
 	public bool isClicked = false;
 	public bool onCaptureArea = false;
 
+	private SpriteRenderer spriteRenderer;
+
 	void Start(){
+		if(value == 0 ) value = Random.Range(1, 3);
 		isClicked = false;
+		spriteRenderer = GetComponent<SpriteRenderer>();
 	}
 
 	void Update(){
-
+		spriteRenderer.sprite = pieceGraphics[value];
 		renderer.material.color = (isClicked)? Color.yellow : Color.white;
 		renderer.material.color = (onCaptureArea)? Color.red : renderer.material.color;
 	}
@@ -21,28 +26,24 @@ public class Piece : MonoBehaviour {
 		Debug.Log("Click : " + gameObject.name);
 		if(!GameManager.g.moveLock){
 			if(!isClicked){
-				GameManager.g.AddPiece(transform);
+				GameManager.g.AddPieceToSeq(transform);
+				isClicked = true;
 			}
 			else {
-				if(GameManager.g.SequenceCount() > 0){
-					if(transform == GameManager.g.pieceSequence[0] &&
-					   GameManager.g.SequenceCount() > 2){
-						GameManager.g.StartCoroutine("CompleteSequence", transform);
-					}
-					else {
-						GameManager.g.RemovePiece(transform);
-						isClicked = false;
-					}
+				if(transform == GameManager.g.pieceSequence[0] &&
+				   GameManager.g.SeqCount() > 2){
+					GameManager.g.StartCoroutine("CompleteSeq", transform);
+				}
+				else {
+					GameManager.g.RemovePieceFromSeq(transform);
 				}
 			}
 		}
 	}
 
 	void OnTriggerEnter2D(Collider2D info){
-		Debug.Log("Colisao");
 		if(info.gameObject.GetComponent<PolygonCollider2D>() != null){
-			Debug.Log("Colisao PolCollider");
-			onCaptureArea = true;
+			onCaptureArea = !isClicked;
 		}
 	}
 }
